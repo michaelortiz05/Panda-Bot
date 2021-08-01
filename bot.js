3fde5733a8f0fa8855f7bbd1ae38700944e095d5
michael <michaelortiz05@hotmail.com>
2021-08-01 12:04:21 -0400

    Added basic command handling and bot pinging

--- bot.js
+++ bot.js
@@ -1,11 +1,41 @@
 'use strict'
 
-const Discord = require('discord.js');
-const auth = require('./Auth/bot_token.json');
-const client = new Discord.Client();
+const fs = require('fs');
 
+// Discord modules
+const Discord = require('discord.js');
+const { token, prefix } = require('./Auth/bot_token.json');
+
+// Load bot and commands
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// Set commands
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+commandFiles.forEach(file => {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+});
+
+// Output when bot is ready
 client.on('ready', () => {
     console.log('Bot loaded');
 });
 
-client.login(auth['token']);
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(token);
